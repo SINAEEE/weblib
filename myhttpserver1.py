4b0b423c20a168764208a89d4a11e40b9f2d5c48
SINAEEE <kimshin1227@naver.com>
2018-06-26 14:45:21 +0900

    myhttpserver1 fin

---
+++ myhttpserver1.py
@@ -0,0 +1,19 @@
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+port = 9000
+class MyHttpReqeustHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type','text/html: charset=utf-8')
+        print('receive request')
+        self.end_headers()
+        self.wfile.write('<h1>Hello World\n</h1>'.encode('utf-8'))
+
+
+
+#서버 구동
+httpd = HTTPServer(('',9000),MyHttpReqeustHandler)
+print('HTTP server starts....')
+httpd.serve_forever()
