2c57ac29a5f5ad6654943a95a77b6ce8c817b491
SINAEEE <kimshin1227@naver.com>
2018-06-26 13:25:14 +0900

    Initial commit

---
+++ ex2-1.py
@@ -0,0 +1,13 @@
+
+#url parse
+
+from urllib.parse import urlparse
+#urlsplit(), urljoin(), parse_qs()
+
+url='http://www.python.org:80/guido/python.html:philosophy?a=10#here'
+result = urlparse(url)
+print(result)
+
+
+
+
